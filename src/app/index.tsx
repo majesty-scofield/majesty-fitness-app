5ae597d14a26bfa62ae22620a3942d7d8d58ca08
majesty-scofield <reborndzre@gmail.com>
2024-04-05 10:16:35 +0000

    Stepzen API & search query

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
-import {StyleSheet, View, FlatList} from 'react-native';
+import {StyleSheet, View, FlatList, TextInput, Button} from 'react-native';
 import FoodListItem from "../components/FoodListItem";
+import {useState} from "react";
 
 const foodItems = [
     {label: "Pizza", cal: 75, brand: 'Dominos'},
@@ -8,8 +9,16 @@ const foodItems = [
 ]
 
 export default function Index() {
+    const [search, setSearch] = useState('')
+
+    const performSearch = () => {
+        console.warn('Searching for food list:', search)
+        setSearch('')
+    }
     return (
         <View style={styles.container}>
+            <TextInput value={search} onChangeText={setSearch} placeholder="Search..." style={styles.input} />
+            {search && <Button title='Search' onPress={performSearch}/>}
             <FlatList
                 data={foodItems}
                 renderItem={({item}) => <FoodListItem item={item} />}
@@ -24,7 +33,13 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         padding: 10,
-        gap: 5
+        gap: 10
     },
+
+    input: {
+        backgroundColor: '#f2f2f2',
+        padding: 10,
+        borderRadius: 20,
+    }
 });
 
