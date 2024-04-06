248864719775f023f7d6ca8c7ae63ae6ba8068d2
majesty-scofield <reborndzre@gmail.com>
2024-04-06 07:17:34 +0000

    Food logger UI

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,77 +1,84 @@
-import {StyleSheet, View, FlatList, TextInput, Button, ActivityIndicator, Text} from 'react-native';
+import { View, Text, FlatList, StyleSheet, Button } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
 import FoodListItem from "../components/FoodListItem";
-import {useState} from "react";
-import {gql, useLazyQuery} from "@apollo/client";
-
-const query = gql`
-    query search($ingr: String) {
-        search(ingr: $ingr) {
-            text
-            hints {
-                food {
-                    label
-                    brand
-                    foodId
-                    nutrients {
-                        ENERC_KCAL
-                    }
-                }
-            }
-        }
-    }
-`
 
 const foodItems = [
-    {label: "Pizza", cal: 75, brand: 'Dominos'},
-    {label: "Apple", cal: 50, brand: 'Generic'},
-    {label: "Coffee", cal: 100, brand: 'Americano'},
-]
+  {
+    food: {
+      label: "Apple",
+      nutrients: { ENERC_KCAL: 122 },
+      brand: "Apple Brands",
+    },
+  },
+  {
+    food: {
+      label: "Banana",
+      nutrients: { ENERC_KCAL: 3424 },
+      brand: "Banana Brands",
+    },
+  },
+  {
+    food: {
+      label: "Orange",
+      nutrients: { ENERC_KCAL: 433 },
+      brand: "Orange Brands",
+    },
+  },
+  {
+    food: {
+      label: "Pineapple",
+      nutrients: { ENERC_KCAL: 34 },
+      brand: "Pineapple Brands",
+    },
+  },
+  {
+    food: {
+      label: "Grapes",
+      nutrients: { ENERC_KCAL: 4665 },
+      brand: "Grapes Brands",
+    },
+  },
+];
 
-export default function Index() {
-    const [search, setSearch] = useState('')
-
-    const [runSearch, {loading, error, data}] = useLazyQuery(query)
-
-    if (error) return <Text>Failed to search :(</Text>;
-    
-
-    const performSearch = () => {
-        runSearch({
-          variables: {
-            ingr: search,
-          },
-        });
-    }
-
-    const items = data?.search?.hints || []
-
-    return (
-        <View style={styles.container}>
-            <TextInput value={search} onChange={performSearch} onChangeText={setSearch} placeholder="Search..." style={styles.input} />
-            {search && <Button title='Search' onPress={performSearch}/>}
-            {loading && <ActivityIndicator size="large" color="#0000ff" />}
-            <FlatList
-                data={items}
-                renderItem={({item}) => <FoodListItem item={item} />}
-                ListEmptyComponent={() => <Text>Search a food</Text>}
-                contentContainerStyle={{gap: 5}}
-            />
-        </View>
-    );
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <Text style={styles.title}>Calories Remaining</Text>
+        <Text style={styles.title}>1779 - 532 = 650</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.title}>Today's food</Text>
+        <Link href="/search" asChild>
+          <Button title="Add Food" />
+        </Link>
+      </View>
+      <FlatList
+        data={foodItems}
+        renderItem={({ item }) => <FoodListItem item={item} />}
+        contentContainerStyle={{ gap: 5 }}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        padding: 10,
-        gap: 10
-    },
-
-    input: {
-        backgroundColor: '#f2f2f2',
-        padding: 10,
-        borderRadius: 20,
-    }
+  container: {
+    backgroundColor: "#fff",
+    padding: 10,
+    flex: 1,
+    gap: 10,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "500",
+    flex: 1,
+    color: "dimgray",
+  },
 });
-
---
+++ src/app/search.tsx
@@ -0,0 +1,76 @@
+import {StyleSheet, View, FlatList, TextInput, Button, ActivityIndicator, Text} from 'react-native';
+import FoodListItem from "../components/FoodListItem";
+import {useState} from "react";
+import {gql, useLazyQuery} from "@apollo/client";
+
+const query = gql`
+    query search($ingr: String) {
+        search(ingr: $ingr) {
+            text
+            hints {
+                food {
+                    label
+                    brand
+                    foodId
+                    nutrients {
+                        ENERC_KCAL
+                    }
+                }
+            }
+        }
+    }
+`
+
+export default function SearchScreen() {
+  const [search, setSearch] = useState("");
+
+  const [runSearch, { loading, error, data }] = useLazyQuery(query);
+
+  if (error) return <Text>Failed to search :(</Text>;
+
+  const performSearch = () => {
+    runSearch({
+      variables: {
+        ingr: search,
+      },
+    });
+  };
+
+  const items = data?.search?.hints || [];
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        value={search}
+        onChange={performSearch}
+        onChangeText={setSearch}
+        placeholder="Search..."
+        style={styles.input}
+      />
+      {search && <Button title="Search" onPress={performSearch} />}
+      {loading && <ActivityIndicator size="large" color="#0000ff" />}
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <FoodListItem item={item} />}
+        ListEmptyComponent={() => <Text>Search a food</Text>}
+        contentContainerStyle={{ gap: 5 }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        padding: 10,
+        gap: 10
+    },
+
+    input: {
+        backgroundColor: '#f2f2f2',
+        padding: 10,
+        borderRadius: 20,
+    }
+});
+
